b4a8e831adf4f53856fb97d254b77a81c27a309c
tringcooler <tringcooler@users.noreply.github.com>
2017-12-17 01:58:48 +0800

    standard commit

--- 7day_sim.js
+++ 7day_sim.js
@@ -775,10 +775,10 @@ var sim7 = (function() {
         var glb_elm = this._makelog_glb();
         var pos_elm = this._makelog_pos();
         var exec_elm = this._makelog_exec(rprt)
-        elm.append(time_elm)
-            .append(glb_elm)
+        elm.append(glb_elm)
             .append(pos_elm)
-            .append(exec_elm);
+            .append(exec_elm)
+            .append(time_elm);
         if(this.sim.act_point() == 24) {
             elm.addClass('day_start');
         }
